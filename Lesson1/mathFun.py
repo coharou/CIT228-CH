eeefc74d8e7c29cd710c3a4dca483a77ab33fcde
Colin Haroutunian <60243200+coharou@users.noreply.github.com>
2021-01-19 16:04:07 -0500

    Add files via upload

---
+++ Lesson1/mathFun.py
@@ -0,0 +1,11 @@
+numX = 20
+stringX = str(numX)
+result1 = numX * 10
+result2 = stringX * 10
+print("Result 1 =", result1)
+print("Result 2 =", result2)
+
+value1 = 30
+value2 = str(value1)
+print(type(value1))
+print(type(value2))
---
+++ Lesson1/stringFun.py
@@ -0,0 +1,38 @@
+print("\n")
+print("Exercise 1")
+name = "colin haroutunian"
+print(name.title())
+print(name.upper())
+print(name.lower())
+print("First initial: ", name[0].upper())
+print()
+print("Exercise 2")
+noun = "double-quotes"
+adj = "long"
+verb1 = "use"
+verb2 = "write"
+ending = "lines of strings."
+print("Concatenation: \t" + verb1.title() + " three " + noun + " to " + verb2 + " " + adj + " " + ending)
+print(f"F-string: \t{verb1.title()} three {noun} to {verb2} {adj} {ending}")
+print()
+print("Exercise 3")
+quote = """'They sat and stared out of the window.
+They were on a street which seemed to run on forever, 
+mile after mile -- thirty-four of them, 
+if they had known it -- and each side of it one 
+uninterrupted row of wretched little two-story frame buildings.
+Down every side street they could see, it was the same --
+never a hill and never a hollow, but always the same
+endless vista of ugly and dirty little wooden buildings.'
+-- The Jungle """
+print(quote)
+print()
+print("Exercise 4")
+print("Chapters\tPage Number")
+print("I\t\t3")
+print("II\t\t24")
+print("III\t\t35")
+print("IV\t\t48")
+print("V\t\t61")
+print("VI\t\t72")
+print()
